cd20ec785b7688aa12799aab6189d1655d3d6ca7
Stolle42 <simon.stoll@arcor.de>
2023-03-03 21:56:25 +0100

    fixed issue #1

--- pg_calculation/calc_http.py
+++ pg_calculation/calc_http.py
@@ -8,18 +8,16 @@ def top():
    return flask.render_template("top.html",data=topscorer)
    
 
-@app.route('/correct/<int:answer>')
-def correct(answer):
-    return flask.render_template("result.html",answer=answer,result=23*7)
-
-@app.route('/',methods=['POST','GET'])
+@app.route('/')
 def startpage():
+   return flask.render_template("calc.html")
+   
+@app.route('/result',methods = ['POST', 'GET'])
+def result():
    if flask.request.method == 'POST':
-      uc = flask.request.form['usercalc']
-      return flask.redirect(flask.url_for('correct',answer = uc))
-   else:
-      uc = flask.request.args.get('usercalc')
-      return flask.redirect(flask.url_for('correct', answer=uc))
+      user_answers = flask.request.form
+      #TODO: add a1!!!! and remove that ugly "161"
+      return flask.render_template("result.html",answers = user_answers,results={"m1":"161"})
 
 if __name__=="__main__":
     app.run(host='localhost',port=5000, debug = True) #What does that mean? Set to ‘0.0.0.0’ to have server available externally
