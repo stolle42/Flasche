66a7f7a9425ce5334aae939d930699d2d27f166a
Stolle42 <simon.stoll@arcor.de>
2023-03-11 22:06:57 +0100

    change name cookie to level

--- pg_calculation/calc_http.py
+++ pg_calculation/calc_http.py
@@ -1,5 +1,7 @@
 import flask
 # from calclib import get_mult
+from flask import Flask, render_template, request, make_response
+
 app=flask.Flask(__name__)
 
 @app.route('/top')
@@ -12,26 +14,26 @@ def top():
 def startpage():
    print("all cookies:")
    print(flask.request.cookies)
-   return flask.render_template("calc.html",name=flask.request.cookies.get('username'))
+   return flask.render_template("calc.html",level=flask.request.cookies.get('level'))
 
-@app.route('/name')
-def name():
-   return flask.render_template("setname.html")
+@app.route('/level')
+def level():
+   return flask.render_template("setlevel.html")
 
-@app.route('/namecookie',methods = ['POST'])
-def set_name():
-   print("seems to be working")
-   name=flask.request.form['username']
-   response=flask.make_response(flask.redirect(flask.url_for("startpage")))
-   response.set_cookie('username',name)
+@app.route('/setcookie', methods = ['POST'])
+def setcookie():
+   response=flask.make_response( flask.redirect(flask.url_for("startpage")))
+   response.set_cookie('level', request.form['level_form'])
+   
    return response
    
 @app.route('/result',methods = ['POST', 'GET'])
 def result():
    if flask.request.method == 'POST':
       user_answers = flask.request.form
-      #TODO: add a1!!!! and remove that ugly "161"
-      return flask.render_template("result.html",answers = user_answers,results={"m1":"161"})
+      response=flask.make_response( flask.render_template("result.html",answers = user_answers,results={"m1":"161"}))
+      response.set_cookie('level',user_answers["m1"])
+      return response
 
 if __name__=="__main__":
     app.run(host='localhost',port=5000, debug = True) #What does that mean? Set to ‘0.0.0.0’ to have server available externally
